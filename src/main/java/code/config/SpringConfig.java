c307b9476eed9a31ce263b16de2150b9c5d56a81
Denis Bragin <denis.bragin@sap.com>
2025-08-21 22:10:13 +0600

    chore: autoformat

--- src/main/java/code/config/SpringConfig.java
+++ src/main/java/code/config/SpringConfig.java
@@ -6,11 +6,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.*;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -19,8 +15,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-import javax.sql.DataSource;
-import java.util.Objects;
 
 @Configuration
 @ComponentScan("code")
@@ -29,65 +23,65 @@ import java.util.Objects;
 @PropertySource("classpath:database.properties")
 public class SpringConfig implements WebMvcConfigurer {
 
-    private final ApplicationContext applicationContext;
+  private final ApplicationContext applicationContext;
 
-    @Value("${db.driver}")
-    private String driver;
+  @Value("${db.driver}")
+  private String driver;
 
-    @Value("${db.url}")
-    private String url;
+  @Value("${db.url}")
+  private String url;
 
-    @Value("${db.username}")
-    private String username;
+  @Value("${db.username}")
+  private String username;
 
-    @Value("${db.password}")
-    private String password;
+  @Value("${db.password}")
+  private String password;
 
-    @Autowired
-    public SpringConfig(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
-    }
+  @Autowired
+  public SpringConfig(ApplicationContext applicationContext) {
+    this.applicationContext = applicationContext;
+  }
 
-    @Bean
-    public SpringResourceTemplateResolver templateResolver() {
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setApplicationContext(applicationContext);
-        templateResolver.setPrefix("/WEB-INF/views/");
-        templateResolver.setSuffix(".html");
-        return templateResolver;
-    }
+  @Bean
+  public SpringResourceTemplateResolver templateResolver() {
+    SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+    templateResolver.setApplicationContext(applicationContext);
+    templateResolver.setPrefix("/WEB-INF/views/");
+    templateResolver.setSuffix(".html");
+    return templateResolver;
+  }
 
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver());
-        templateEngine.setEnableSpringELCompiler(true);
-        return templateEngine;
-    }
+  @Bean
+  public SpringTemplateEngine templateEngine() {
+    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+    templateEngine.setTemplateResolver(templateResolver());
+    templateEngine.setEnableSpringELCompiler(true);
+    return templateEngine;
+  }
 
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-        dataSource.setDriverClassName(Objects.requireNonNull(driver));
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
+    dataSource.setDriverClassName(Objects.requireNonNull(driver));
+    dataSource.setUrl(url);
+    dataSource.setUsername(username);
+    dataSource.setPassword(password);
 
-        return dataSource;
-    }
+    return dataSource;
+  }
 
-    @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return new JdbcTemplate(dataSource());
-    }
+  @Bean
+  public JdbcTemplate jdbcTemplate() {
+    return new JdbcTemplate(dataSource());
+  }
 
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-        resolver.setTemplateEngine(templateEngine());
-        registry.viewResolver(resolver);
-    }
+  @Override
+  public void configureViewResolvers(ViewResolverRegistry registry) {
+    ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+    resolver.setTemplateEngine(templateEngine());
+    registry.viewResolver(resolver);
+  }
 
   @Override
   public void addInterceptors(
--- src/main/java/code/controllers/PeopleController.java
+++ src/main/java/code/controllers/PeopleController.java
@@ -3,6 +3,7 @@ package code.controllers;
 import code.dao.PersonDAO;
 import code.models.Person;
 import code.util.PersonValidator;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,68 +16,70 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.validation.Valid;
-
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
-    @Autowired
-    private PersonDAO personDAO;
-    @Autowired
-    private PersonValidator personValidator;
+  @Autowired private PersonDAO personDAO;
+  @Autowired private PersonValidator personValidator;
 
-    @GetMapping()
-    public String index(Model model) {
-        model.addAttribute("people", personDAO.index());
-        return "people/index";
+  @GetMapping("/test")
+  public String test(Model model) {
+    model.addAttribute("person", personDAO.testInternalCall());
+    return "people/show";
+  }
+
+  @GetMapping()
+  public String index(Model model) {
+    model.addAttribute("people", personDAO.index());
+    return "people/index";
+  }
+
+  @GetMapping("/{id}")
+  public String show(@PathVariable("id") int id, Model model) {
+    model.addAttribute("person", personDAO.show(id));
+    return "people/show";
+  }
+
+  @GetMapping("/new")
+  public String newPerson(@ModelAttribute("person") Person person) {
+    return "people/new";
+  }
+
+  @PostMapping()
+  public String create(@ModelAttribute @Valid Person person, BindingResult bindingResult) {
+    personValidator.validate(person, bindingResult);
+
+    if (bindingResult.hasErrors()) {
+      return "/people/new";
     }
 
-    @GetMapping("/{id}")
-    public String show(@PathVariable("id") int id, Model model) {
-        model.addAttribute("person", personDAO.show(id));
-        return "people/show";
+    personDAO.save(person);
+    return "redirect:/people";
+  }
+
+  @GetMapping("/{id}/edit")
+  public String edit(@PathVariable("id") int id, Model model) {
+    model.addAttribute("person", personDAO.show(id));
+    return "people/edit";
+  }
+
+  @PatchMapping("/{id}")
+  public String update(
+      @ModelAttribute("person") @Valid Person person,
+      BindingResult bindingResult,
+      @PathVariable("id") int id) {
+    personValidator.validate(person, bindingResult);
+
+    if (bindingResult.hasErrors()) {
+      return "people/edit";
     }
+    personDAO.update(id, person);
+    return "redirect:/people";
+  }
 
-    @GetMapping("/new")
-    public String newPerson(@ModelAttribute("person") Person person) {
-        return "people/new";
-    }
-
-    @PostMapping()
-    public String create(@ModelAttribute @Valid Person person,
-                         BindingResult bindingResult) {
-        personValidator.validate(person, bindingResult);
-
-        if (bindingResult.hasErrors()) {
-            return "/people/new";
-        }
-
-        personDAO.save(person);
-        return "redirect:/people";
-    }
-
-    @GetMapping("/{id}/edit")
-    public String edit(@PathVariable("id") int id, Model model) {
-        model.addAttribute("person", personDAO.show(id));
-        return "people/edit";
-    }
-
-    @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") @Valid Person person,
-                         BindingResult bindingResult,
-                         @PathVariable("id") int id) {
-        personValidator.validate(person, bindingResult);
-
-        if (bindingResult.hasErrors()) {
-            return "people/edit";
-        }
-        personDAO.update(id, person);
-        return "redirect:/people";
-    }
-
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id) {
-        personDAO.delete(id);
-        return "redirect:/people";
-    }
+  @DeleteMapping("/{id}")
+  public String delete(@PathVariable("id") int id) {
+    personDAO.delete(id);
+    return "redirect:/people";
+  }
 }
